0cbf1ce7740c94a2e14daea8461b507765e8c4b1
EpicSquid <epicsquid315@gmail.com>
2022-08-06 10:14:09 +1000

    Minor fluid stuff

--- src/main/java/net/joefoxe/hexerei/fluid/BloodFluid.java
+++ src/main/java/net/joefoxe/hexerei/fluid/BloodFluid.java
@@ -2,115 +2,102 @@ package net.joefoxe.hexerei.fluid;
 
 import net.joefoxe.hexerei.item.ModItems;
 import net.joefoxe.hexerei.particle.ModParticleTypes;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.LiquidBlock;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 
+import javax.annotation.Nonnull;
 import java.util.Random;
-
-import net.minecraftforge.fluids.ForgeFlowingFluid.Properties;
 
 public abstract class BloodFluid extends ForgeFlowingFluid {
 
-    @Override
-    protected void randomTick(Level world, BlockPos pos, FluidState state, Random random) {
-        super.randomTick(world, pos, state, random);
-    }
+	protected BloodFluid(Properties properties) {
+		super(properties);
+	}
 
-    @Override
-    public void tick(Level worldIn, BlockPos pos, FluidState state) {
+	@Override
+	public Fluid getFlowing() {
+		return ModFluids.BLOOD_FLOWING.get();
+	}
 
-        super.tick(worldIn, pos, state);
-    }
+	@Override
+	public Fluid getSource() {
+		return ModFluids.BLOOD_FLUID.get();
+	}
 
-    protected BloodFluid(Properties properties) {
+	@Override
+	protected boolean canConvertToSource() {
+		return false;
+	}
 
-        super(properties);
-    }
+	@Override
+	protected void beforeDestroyingBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
+		BlockEntity tileentity = state.hasBlockEntity() ? worldIn.getBlockEntity(pos) : null;
+		Block.dropResources(state, worldIn, pos, tileentity);
+	}
 
-    @Override
-    public Fluid getFlowing() {
-        return ModFluids.BLOOD_FLOWING.get();
-    }
+	@Override
+	protected int getSlopeFindDistance(LevelReader worldIn) {
+		return 4;
+	}
 
-    @Override
-    public Fluid getSource() {
-        return ModFluids.BLOOD_FLUID.get();
-    }
+	@Override
+	protected int getDropOff(LevelReader worldIn) {
+		return 2;
+	}
 
-    @Override
-    protected boolean canConvertToSource() {
-        return false;
-    }
+	@Override
+	public Item getBucket() {
+		return ModItems.BLOOD_BUCKET.get();
+	}
 
-    @Override
-    protected void beforeDestroyingBlock(LevelAccessor worldIn, BlockPos pos, BlockState state) {
-        BlockEntity tileentity = state.hasBlockEntity() ? worldIn.getBlockEntity(pos) : null;
-        Block.dropResources(state, worldIn, pos, tileentity);
-    }
+	@Override
+	protected boolean canBeReplacedWith(FluidState fluidState, BlockGetter blockReader, BlockPos pos, Fluid fluid, Direction direction) {
+		return direction == Direction.DOWN && !fluid.is(FluidTags.WATER);
+	}
 
-    @Override
-    protected int getSlopeFindDistance(LevelReader worldIn) {
-        return 4;
-    }
+	@Override
+	public int getTickDelay(LevelReader p_205569_1_) {
+		return 5;
+	}
 
-    @Override
-    protected int getDropOff(LevelReader worldIn) {
-        return 2;
-    }
+	@Override
+	protected float getExplosionResistance() {
+		return 100f;
+	}
 
-    @Override
-    public Item getBucket() {
-        return ModItems.BLOOD_BUCKET.get();
-    }
+	@Override
+	protected BlockState createLegacyBlock(FluidState state) {
+		return ModFluids.BLOOD_BLOCK.get().defaultBlockState().setValue(LiquidBlock.LEVEL, Integer.valueOf(getLegacyLevel(state)));
+	}
 
-    @Override
-    protected boolean canBeReplacedWith(FluidState fluidState, BlockGetter blockReader, BlockPos pos, Fluid fluid, Direction direction) {
-        return direction == Direction.DOWN && !fluid.is(FluidTags.WATER);
-    }
+	@Override
+	public boolean isSource(@Nonnull FluidState state) {
+		return false;
+	}
 
-    @Override
-    public int getTickDelay(LevelReader p_205569_1_) {
-        return 5;
-    }
-
-    @Override
-    protected float getExplosionResistance() {
-        return 100f;
-    }
-
-    @Override
-    protected BlockState createLegacyBlock(FluidState state) {
-        return ModFluids.BLOOD_BLOCK.get().defaultBlockState().setValue(LiquidBlock.LEVEL, Integer.valueOf(getLegacyLevel(state)));
-    }
-
-    @Override
-    public boolean isSource(FluidState state) {
-        return false;
-    }
-
-    @Override
-    public int getAmount(FluidState state) {
-        return 0;
-    }
+	@Override
+	public int getAmount(@Nonnull FluidState state) {
+		return 0;
+	}
 
 
 //    public boolean isEquivalentTo(Fluid fluidIn) {
@@ -119,68 +106,68 @@ public abstract class BloodFluid extends ForgeFlowingFluid {
 //
 
 
-    public static class Flowing extends ForgeFlowingFluid {
-        protected Flowing(Properties properties) {
-            super(properties);
-        }
+	public static class Flowing extends ForgeFlowingFluid {
+		protected Flowing(Properties properties) {
+			super(properties);
+		}
 
-        protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
-            super.createFluidStateDefinition(builder);
-            builder.add(LEVEL);
-        }
+		protected void createFluidStateDefinition(@Nonnull StateDefinition.Builder<Fluid, FluidState> builder) {
+			super.createFluidStateDefinition(builder);
+			builder.add(LEVEL);
+		}
 
-        public int getAmount(FluidState state) {
-            return state.getValue(LEVEL);
-        }
+		public int getAmount(FluidState state) {
+			return state.getValue(LEVEL);
+		}
 
-        public boolean isSource(FluidState state) {
-            return false;
-        }
+		public boolean isSource(@Nonnull FluidState state) {
+			return false;
+		}
 
-        @OnlyIn(Dist.CLIENT)
-        public void animateTick(Level worldIn, BlockPos pos, FluidState state, Random random) {
-            if (!state.isSource() && !state.getValue(FALLING)) {
-                if (random.nextInt(64) == 0) {
-                    worldIn.playSound(null, (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, SoundEvents.WATER_AMBIENT, SoundSource.BLOCKS, random.nextFloat() * 0.25F + 0.75F, random.nextFloat() + 0.5F);
-                }
-            } else if (random.nextInt(12) == 0) {
-                if (random.nextInt(3) == 0)
-                    worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8)), (double)pos.getZ() + random.nextDouble(), -0.01D+(random.nextDouble()*0.02), -0.02D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02));
-                worldIn.addParticle(ModParticleTypes.BLOOD_BIT.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8)), (double)pos.getZ() + random.nextDouble(), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02));
+		@OnlyIn(Dist.CLIENT)
+		public void animateTick(Level worldIn, BlockPos pos, FluidState state, Random random) {
+			if (!state.isSource() && !state.getValue(FALLING)) {
+				if (random.nextInt(64) == 0) {
+					worldIn.playSound(null, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, SoundEvents.WATER_AMBIENT, SoundSource.BLOCKS, random.nextFloat() * 0.25F + 0.75F, random.nextFloat() + 0.5F);
+				}
+			} else if (random.nextInt(12) == 0) {
+				if (random.nextInt(3) == 0)
+					worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double) pos.getX() + random.nextDouble(), (double) pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8.0f)), (double) pos.getZ() + random.nextDouble(), -0.01D + (random.nextDouble() * 0.02), -0.02D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02));
+				worldIn.addParticle(ModParticleTypes.BLOOD_BIT.get(), (double) pos.getX() + random.nextDouble(), (double) pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8.0f)), (double) pos.getZ() + random.nextDouble(), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02));
 
-            }
-            //worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8)), (double)pos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
-        }
-    }
+			}
+			//worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble() * (state.getValue(LEVEL) / 8)), (double)pos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
+		}
+	}
 
-    public static class Source extends ForgeFlowingFluid {
-        protected Source(Properties properties) {
-            super(properties);
-        }
+	public static class Source extends ForgeFlowingFluid {
+		protected Source(Properties properties) {
+			super(properties);
+		}
 
-        public int getAmount(FluidState state) {
-            return 8;
-        }
+		public int getAmount(@Nonnull FluidState state) {
+			return 8;
+		}
 
-        public boolean isSource(FluidState state) {
-            return true;
-        }
+		public boolean isSource(@Nonnull FluidState state) {
+			return true;
+		}
 
-        @OnlyIn(Dist.CLIENT)
-        public void animateTick(Level worldIn, BlockPos pos, FluidState state, Random random) {
-            if (!state.isSource() && !state.getValue(FALLING)) {
-                if (random.nextInt(64) == 0) {
-                    worldIn.playSound(null, (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, SoundEvents.WATER_AMBIENT, SoundSource.BLOCKS, random.nextFloat() * 0.25F + 0.75F, random.nextFloat() + 0.5F);
-                }
-            } else if (random.nextInt(14) == 0) {
-                if (random.nextInt(2) == 0)
-                    worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble()), (double)pos.getZ() + random.nextDouble(), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02));
-                worldIn.addParticle(ModParticleTypes.BLOOD_BIT.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + (random.nextDouble()), (double)pos.getZ() + random.nextDouble(), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02), -0.01D+(random.nextDouble()*0.02));
+		@OnlyIn(Dist.CLIENT)
+		public void animateTick(Level worldIn, BlockPos pos, FluidState state, Random random) {
+			if (!state.isSource() && !state.getValue(FALLING)) {
+				if (random.nextInt(64) == 0) {
+					worldIn.playSound(null, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, SoundEvents.WATER_AMBIENT, SoundSource.BLOCKS, random.nextFloat() * 0.25F + 0.75F, random.nextFloat() + 0.5F);
+				}
+			} else if (random.nextInt(14) == 0) {
+				if (random.nextInt(2) == 0)
+					worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double) pos.getX() + random.nextDouble(), (double) pos.getY() + (random.nextDouble()), (double) pos.getZ() + random.nextDouble(), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02));
+				worldIn.addParticle(ModParticleTypes.BLOOD_BIT.get(), (double) pos.getX() + random.nextDouble(), (double) pos.getY() + (random.nextDouble()), (double) pos.getZ() + random.nextDouble(), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02), -0.01D + (random.nextDouble() * 0.02));
 
-            }
-            //worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + random.nextDouble(), (double)pos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
-        }
+			}
+			//worldIn.addParticle(ModParticleTypes.BLOOD.get(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + random.nextDouble(), (double)pos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
+		}
 
-    }
+	}
 
 }
